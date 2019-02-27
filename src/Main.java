5b90816ae33197b496188aec9d96bec630e3c8ac
micky1608 <mteaudors@outlook.com>
2019-02-27 15:50:09 +0100

    Read data from file function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\"youhou test\"");
+        String s = new String();
+        try {
+            s = Util.getTextFile("data/data_test");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(s);
     }
 }
---
+++ src/Util.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Util {
+
+    public static String getTextFile(String path) throws IOException {
+        File file = new File(path);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        StringBuilder buffer = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null)
+            buffer.append(line + "\n");
+
+        return buffer.toString();
+    }
+}
