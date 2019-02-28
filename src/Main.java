9d08d787104353d2e2c4637330b03f6f10071b4e
micky1608 <mteaudors@outlook.com>
2019-02-28 19:14:15 +0100

    Photo class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 
 }
--- src/Photo.java
+++ src/Photo.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Photo {
+
+    private int id;
+
+    // photo orientation : true = horizontal , false = vertical
+    private boolean isHorizontal;
+
+    private ArrayList<String> tags;
+
+    public Photo(int id , boolean isHorizontal , ArrayList<String> tags) {
+        this.id = id;
+        this.isHorizontal = isHorizontal;
+        this.tags = tags;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isHorizontal() {
+        return isHorizontal;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+    
+
+
+
 }
