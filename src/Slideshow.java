69afabae2cdc04999b2ca3467218438c22d01cbc
YValeri <44030943+YValeri@users.noreply.github.com>
2019-02-28 19:38:27 +0100

    Add files via upload

--- src/Slideshow.java
+++ src/Slideshow.java
@@ -1,31 +1,30 @@
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Slideshow {
-	ArrayList<Photo> l;
+	ArrayList<Slide> slides;
 	int score;
 	
 	public Slideshow() {
-		l = new ArrayList<>();
+		slides = new ArrayList<>();
 		score = 0;
 	}
 	
-	public Slideshow(ArrayList<Photo> l) {
-		this.l = l;
-		score = score();
+	public Slideshow(ArrayList<Slide> l) {
+		this.slides = l;
+		score = computeScore();
 	}
 	
 	public int computeScore() {
-		for(int i = 0; i < l.size()-1; ++i)
-			score += l.get(i).computeScore(l.get(i+1));
+		for(int i = 0; i < slides.size()-1; ++i)
+			score += slides.get(i).computeScore(slides.get(i+1));
 	}
 
 
 	public void shuffle(int ind1, int ind2){
 		//Calculate score before swapping
-		Slide tempSlide = l.get(ind1);
-		l.set(ind1, l.get(ind2));
-		l.set(ind2, tempSlide);
+		Slide tempSlide = slides.get(ind1);
+		slides.set(ind1, slides.get(ind2));
+		slides.set(ind2, tempSlide);
 	}
 }
