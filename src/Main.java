64d3aa699fb11530c4bf159b3d515117e86c159d
Eegan <alex.heintzmann@outlook.fr>
2019-02-28 17:05:08 +0100

    Pushing test - Alex

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\"youhou test\"");
         System.out.println("Test de Dylan !!!\n");
+        System.out.println("Test d'Alex\n");
         String s = new String();
         try {
             s = Util.getTextFile("data/data_test");
